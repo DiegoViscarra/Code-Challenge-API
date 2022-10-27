0c17c7582130902be79014a9d6fdc3b140ef4cad
Diego Viscarra <diego_viscarra@hotmail.es>
2022-10-27 19:10:01 -0400

    [Task-4] Added student service with add method and new DTO model

---
+++ SchedulingAPI/Services/StudentService/IStudentService.cs
@@ -0,0 +1,13 @@
+﻿using SchedulingAPI.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchedulingAPI.Services.StudentService
+{
+    public interface IStudentService
+    {
+        Task<SimpleStudentDTO> AddStudent(SimpleStudentDTO simpleStudentDTO);
+    }
+}
---
+++ SchedulingAPI/Services/StudentService/StudentService.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using SchedulingAPI.Data.Entities;
+using SchedulingAPI.Data.Repositories.StudentRepository;
+using SchedulingAPI.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchedulingAPI.Services.StudentService
+{
+    public class StudentService : IStudentService
+    {
+        private readonly IStudentRepository repository;
+        private readonly IMapper mapper;
+        public StudentService(IStudentRepository repository, IMapper mapper)
+        {
+            this.repository = repository;
+            this.mapper = mapper;
+        }
+
+        public async Task<SimpleStudentDTO> AddStudent(SimpleStudentDTO simpleStudentDTO)
+        {
+            var student = mapper.Map<Student>(simpleStudentDTO);
+            repository.AddStudent(student);
+            if (await repository.SaveChangesAsync())
+                return mapper.Map<SimpleStudentDTO>(student);
+            throw new Exception("Student was not added");
+        }
+    }
+}
