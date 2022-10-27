4ce9d6cfbbcfbd54ee68b0a8a580203689416803
Diego Viscarra <diego_viscarra@hotmail.es>
2022-10-27 15:57:34 -0400

    [Task-2] Created DTO models

---
+++ SchedulingAPI/Models/DTOs/ClassDTO.cs
@@ -0,0 +1,18 @@
+﻿using SchedulingAPI.Data.Entities;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchedulingAPI.Models.DTOs
+{
+    public class ClassDTO
+    {
+        [Required]
+        public int Code { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+
+        public IEnumerable<Registration> Registrations { get; set; }
+    }
+}
