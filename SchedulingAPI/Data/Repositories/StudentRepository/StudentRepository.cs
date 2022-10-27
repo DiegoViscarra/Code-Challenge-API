2c9dc382164e8f684cec9d9924d383f61d7f4429
Diego Viscarra <diego_viscarra@hotmail.es>
2022-10-27 19:07:57 -0400

    [Task-4] Added student repository with add method

---
+++ SchedulingAPI/Data/Repositories/StudentRepository/StudentRepository.cs
@@ -0,0 +1,27 @@
+﻿using SchedulingAPI.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchedulingAPI.Data.Repositories.StudentRepository
+{
+    public class StudentRepository : IStudentRepository
+    {
+        private SchedulingDbContext dbContext;
+        public StudentRepository(SchedulingDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void AddStudent(Student student)
+        {
+            dbContext.Students.Add(student);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return (await dbContext.SaveChangesAsync()) > 0;
+        }
+    }
+}
