9e0d4c70533eab77bfc9aa3336616e258be0372d
Diego Viscarra <diego_viscarra@hotmail.es>
2022-10-27 15:57:09 -0400

    [Task-2] Created entities

---
+++ SchedulingAPI/Data/Entities/Registration.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SchedulingAPI.Data.Entities
+{
+    public class Registration
+    {
+        [ForeignKey("StudentId")]
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+
+        [ForeignKey("Code")]
+        public int Code { get; set; }
+        public Class Class { get; set; }
+    }
+}
---
+++ SchedulingAPI/Data/Entities/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchedulingAPI.Data.Entities
+{
+    public class Student
+    {
+        [Key]
+        [Required]
+        public int StudentId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+
+        public ICollection<Registration> Registrations { get; set; }
+    }
+}
