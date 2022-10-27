993feafe443e5fd39b5c1def77c3f685ea4809a2
Diego Viscarra <diego_viscarra@hotmail.es>
2022-10-27 19:52:26 -0400

    [Task-5] Added update method to student controller

--- SchedulingAPI/Controllers/StudentController.cs
+++ SchedulingAPI/Controllers/StudentController.cs
@@ -19,7 +19,7 @@ namespace SchedulingAPI.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<SimpleStudentDTO>> Post([FromBody] SimpleStudentDTO simpleStudentDTO)
+        public async Task<ActionResult<SimpleStudentDTO>> PostStudent([FromBody] SimpleStudentDTO simpleStudentDTO)
         {
             if (!ModelState.IsValid)
             {
@@ -29,6 +29,24 @@ namespace SchedulingAPI.Controllers
             {
                 var createdStudent = await service.AddStudent(simpleStudentDTO);
                 return Created($"/api/student/{createdStudent.StudentId}", createdStudent);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error:", e);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<SimpleStudentDTO>> PutStudent(int id, [FromBody] SimpleStudentDTO simpleStudentDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var studentUpdated = await service.UpdateStudent(id, simpleStudentDTO);
+                return Ok(studentUpdated);
             }
             catch (Exception e)
             {
