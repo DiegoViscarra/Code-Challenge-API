4d162b9e65a3d3bd1de3833c12bcc5d90c13fc2c
Diego Viscarra <diego_viscarra@hotmail.es>
2022-10-28 12:26:06 -0400

    [Task-8] Added registration repository with registration method

---
+++ SchedulingAPI/Data/Repositories/RegistrationRepository/IRegistrationRepository.cs
@@ -0,0 +1,14 @@
+﻿using SchedulingAPI.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchedulingAPI.Data.Repositories.RegistrationRepository
+{
+    public interface IRegistrationRepository
+    {
+        void AddRegistration(List<Registration> registrations);
+        Task<bool> SaveChangesAsync();
+    }
+}
---
+++ SchedulingAPI/Data/Repositories/RegistrationRepository/RegistrationRepository.cs
@@ -0,0 +1,27 @@
+﻿using SchedulingAPI.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchedulingAPI.Data.Repositories.RegistrationRepository
+{
+    public class RegistrationRepository : IRegistrationRepository
+    {
+        private SchedulingDbContext dbContext;
+        public RegistrationRepository(SchedulingDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void AddRegistration(List<Registration> registrations)
+        {
+            dbContext.Registrations.AddRange(registrations);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return (await dbContext.SaveChangesAsync()) > 0;
+        }
+    }
+}
