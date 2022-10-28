c90f3b0e082934b6afac57d1e768bd6ec2a45fe6
Diego Viscarra <diego_viscarra@hotmail.es>
2022-10-27 20:55:26 -0400

    [Task-6] Added class service with add method

---
+++ SchedulingAPI/Services/ClassService/ClassService.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using SchedulingAPI.Data.Entities;
+using SchedulingAPI.Data.Repositories.ClassRepository;
+using SchedulingAPI.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchedulingAPI.Services.ClassService
+{
+    public class ClassService : IClassService
+    {
+        private readonly IClassRepository repository;
+        private readonly IMapper mapper;
+        public ClassService(IClassRepository repository, IMapper mapper)
+        {
+            this.repository = repository;
+            this.mapper = mapper;
+        }
+
+        public async Task<SimpleClassDTO> AddClass(SimpleClassDTO simpleClassDTO)
+        {
+            var course = mapper.Map<Class>(simpleClassDTO);
+            repository.AddClass(course);
+            if (await repository.SaveChangesAsync())
+                return mapper.Map<SimpleClassDTO>(course);
+            throw new Exception("Class was not added");
+        }
+    }
+}
---
+++ SchedulingAPI/Services/ClassService/IClassService.cs
@@ -0,0 +1,13 @@
+﻿using SchedulingAPI.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchedulingAPI.Services.ClassService
+{
+    public interface IClassService
+    {
+        Task<SimpleClassDTO> AddClass(SimpleClassDTO simpleClassDTO);
+    }
+}
