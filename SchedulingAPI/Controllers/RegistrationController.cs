ec81cde2d8dbace860cb00c9747813d2d4fa94be
Diego Viscarra <diego_viscarra@hotmail.es>
2022-10-28 12:31:42 -0400

    [Task-8] Added registration controller and dependency injection

---
+++ SchedulingAPI/Controllers/RegistrationController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchedulingAPI.Models.DTOs;
+using SchedulingAPI.Services.RegistrationService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchedulingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {
+        private readonly IRegistrationService service;
+        public RegistrationController(IRegistrationService service)
+        {
+            this.service = service;
+        }
+
+        [HttpPost("/students")]
+        public async Task<ActionResult<RegistrationToClassDTO>> PostRegistrationStudents([FromBody] RegistrationToClassDTO registrationToClassDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var createdRegistration = await service.RegisterStudents(registrationToClassDTO);
+                return Created($"/api/registration/students", createdRegistration);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error:", e);
+            }
+        }
+    }
+}
